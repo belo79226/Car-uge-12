1fcc0497ec2a35257af0293e28e093d842a00ed2
Betina <belo79226@edu.ucl.dk>
2026-03-19 12:28:50 +0100

    Add project files.

---
+++ Car uge 12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Car_uge_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
